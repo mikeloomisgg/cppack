4081730ee8397d1242fe88144839e5743890eb5a
Mike Loomis <mikeloomisgg@gmail.com>
2019-06-23 09:33:28 -0500

    Moving specialized templates out of class body

--- msgpack/include/msgpack/msgpack.hpp
+++ msgpack/include/msgpack/msgpack.hpp
@@ -49,6 +49,216 @@ struct is_map<std::map<T, Alloc> > {
   static const bool value = true;
 };
 
+class Packer {
+ public:
+  template<class ... Types>
+  void process(Types &&... args) {
+    std::clog << "Attempting to pack " << sizeof...(args) << " values.\n";
+    (process_type(std::forward<Types>(args)), ...);
+  }
+
+  std::vector<uint8_t> vector() const {
+    return serialized_object;
+  }
+
+ private:
+  std::vector<uint8_t> serialized_object;
+
+  template<class T>
+  void process_type(T &value) {
+    if (is_map<T>::value) {
+      processMap(std::forward<T>(value));
+    } else if (is_container<T>::value) {
+      processArray(std::forward<T>(value));
+    } else {
+      std::clog << "Packing value type: " << "Unknown" << '\n';
+    }
+  }
+
+  template<class T>
+  void processArray(T &array) {
+    std::clog << "Packing value type: " << "object array" << '\n';
+  }
+
+  template<class T>
+  void processMap(T &map) {
+    std::clog << "Packing value type: " << "map" << '\n';
+  }
+};
+
+template<>
+void Packer::process_type(int8_t &value) {
+  std::clog << "Packing value type: " << "int8_t" << '\n';
+}
+
+template<>
+void Packer::process_type(int16_t &value) {
+  std::clog << "Packing value type: " << "int16_t" << '\n';
+}
+
+template<>
+void Packer::process_type(int32_t &value) {
+  std::clog << "Packing value type: " << "int32_t" << '\n';
+}
+
+template<>
+void Packer::process_type(int64_t &value) {
+  std::clog << "Packing value type: " << "int64_t" << '\n';
+}
+
+template<>
+void Packer::process_type(uint8_t &value) {
+  std::clog << "Packing value type: " << "uint8_t" << '\n';
+}
+
+template<>
+void Packer::process_type(uint16_t &value) {
+  std::clog << "Packing value type: " << "uint16_t" << '\n';
+}
+
+template<>
+void Packer::process_type(uint32_t &value) {
+  std::clog << "Packing value type: " << "uint32_t" << '\n';
+}
+
+template<>
+void Packer::process_type(uint64_t &value) {
+  std::clog << "Packing value type: " << "uint64_t" << '\n';
+}
+
+template<>
+void Packer::process_type(std::nullptr_t &value) {
+  std::clog << "Packing value type: " << "std::nullptr_t" << '\n';
+}
+
+template<>
+void Packer::process_type(bool &value) {
+  std::clog << "Packing value type: " << "boolean" << '\n';
+}
+
+template<>
+void Packer::process_type(float &value) {
+  std::clog << "Packing value type: " << "float" << '\n';
+}
+
+template<>
+void Packer::process_type(double &value) {
+  std::clog << "Packing value type: " << "double" << '\n';
+}
+
+template<>
+void Packer::process_type(std::string &value) {
+  std::clog << "Packing value type: " << "string" << '\n';
+}
+
+template<>
+void Packer::process_type(std::vector<uint8_t> &value) {
+  std::clog << "Packing value type: " << "byte array" << '\n';
+}
+
+class Unpacker {
+ public:
+  explicit Unpacker(uint8_t *data_start) {};
+
+  template<class ... Types>
+  void process(Types &&... args) {
+    std::clog << "Attempting to unpack " << sizeof...(args) << " values.\n";
+    (process_type(std::forward<Types>(args)), ...);
+  }
+
+ private:
+  template<class T>
+  void process_type(T &value) {
+    if (is_map<T>::value) {
+      processMap(std::forward<T>(value));
+    } else if (is_container<T>::value) {
+      processArray(std::forward<T>(value));
+    } else {
+      std::clog << "Unpacking value type: " << "Unknown" << '\n';
+    }
+  }
+
+  template<class T>
+  void processArray(T &array) {
+    std::clog << "Unpacking value type: " << "object array" << '\n';
+  }
+
+  template<class T>
+  void processMap(T &map) {
+    std::clog << "Unpacking value type: " << "map" << '\n';
+  }
+};
+
+template<>
+void Unpacker::process_type(int8_t &value) {
+  std::clog << "Unpacking value type: " << "int8_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(int16_t &value) {
+  std::clog << "Unpacking value type: " << "int16_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(int32_t &value) {
+  std::clog << "Unpacking value type: " << "int32_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(int64_t &value) {
+  std::clog << "Unpacking value type: " << "int64_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(uint8_t &value) {
+  std::clog << "Unpacking value type: " << "uint8_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(uint16_t &value) {
+  std::clog << "Unpacking value type: " << "uint16_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(uint32_t &value) {
+  std::clog << "Unpacking value type: " << "uint32_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(uint64_t &value) {
+  std::clog << "Unpacking value type: " << "uint64_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(std::nullptr_t &value) {
+  std::clog << "Unpacking value type: " << "std::nullptr_t" << '\n';
+}
+
+template<>
+void Unpacker::process_type(bool &value) {
+  std::clog << "Unpacking value type: " << "boolean" << '\n';
+}
+
+template<>
+void Unpacker::process_type(float &value) {
+  std::clog << "Unpacking value type: " << "float" << '\n';
+}
+
+template<>
+void Unpacker::process_type(double &value) {
+  std::clog << "Unpacking value type: " << "double" << '\n';
+}
+
+template<>
+void Unpacker::process_type(std::string &value) {
+  std::clog << "Unpacking value type: " << "string" << '\n';
+}
+
+template<>
+void Unpacker::process_type(std::vector<uint8_t> &value) {
+  std::clog << "Unpacking value type: " << "byte array" << '\n';
+}
+
 template<class PackableObject>
 std::vector<uint8_t> pack(PackableObject &&obj) {
   auto vec = std::vector<uint8_t>{};
@@ -64,216 +274,6 @@ UnpackableObject unpack(uint8_t *data_start) {
   obj.pack(unpacker);
   return obj;
 }
-
-class Packer {
- public:
-  template<class ... Types>
-  void process(Types &&... args) {
-    std::clog << "Attempting to pack " << sizeof...(args) << " values.\n";
-    (processType(std::forward<Types>(args)), ...);
-  }
-
-  std::vector<uint8_t> vector() const {
-    return serialized_object;
-  }
-
- private:
-  std::vector<uint8_t> serialized_object;
-
-  template<class T>
-  void processType(T &value) {
-    if (is_map<T>::value) {
-      processMap(std::forward<T>(value));
-    } else if (is_container<T>::value) {
-      processArray(std::forward<T>(value));
-    } else {
-      std::clog << "Packing value type: " << "Unknown" << '\n';
-    }
-  }
-
-  template<>
-  void processType(int8_t &value) {
-    std::clog << "Packing value type: " << "int8_t" << '\n';
-  }
-
-  template<>
-  void processType(int16_t &value) {
-    std::clog << "Packing value type: " << "int16_t" << '\n';
-  }
-
-  template<>
-  void processType(int32_t &value) {
-    std::clog << "Packing value type: " << "int32_t" << '\n';
-  }
-
-  template<>
-  void processType(int64_t &value) {
-    std::clog << "Packing value type: " << "int64_t" << '\n';
-  }
-
-  template<>
-  void processType(uint8_t &value) {
-    std::clog << "Packing value type: " << "uint8_t" << '\n';
-  }
-
-  template<>
-  void processType(uint16_t &value) {
-    std::clog << "Packing value type: " << "uint16_t" << '\n';
-  }
-
-  template<>
-  void processType(uint32_t &value) {
-    std::clog << "Packing value type: " << "uint32_t" << '\n';
-  }
-
-  template<>
-  void processType(uint64_t &value) {
-    std::clog << "Packing value type: " << "uint64_t" << '\n';
-  }
-
-  template<>
-  void processType(std::nullptr_t &value) {
-    std::clog << "Packing value type: " << "std::nullptr_t" << '\n';
-  }
-
-  template<>
-  void processType(bool &value) {
-    std::clog << "Packing value type: " << "boolean" << '\n';
-  }
-
-  template<>
-  void processType(float &value) {
-    std::clog << "Packing value type: " << "float" << '\n';
-  }
-
-  template<>
-  void processType(double &value) {
-    std::clog << "Packing value type: " << "double" << '\n';
-  }
-
-  template<>
-  void processType(std::string &value) {
-    std::clog << "Packing value type: " << "string" << '\n';
-  }
-
-  template<>
-  void processType(std::vector<uint8_t> &value) {
-    std::clog << "Packing value type: " << "byte array" << '\n';
-  }
-
-  template<class T>
-  void processArray(T &array) {
-    std::clog << "Packing value type: " << "object array" << '\n';
-  }
-
-  template<class T>
-  void processMap(T &map) {
-    std::clog << "Packing value type: " << "map" << '\n';
-  }
-};
-
-class Unpacker {
- public:
-  explicit Unpacker(uint8_t *data_start) {};
-
-  template<class ... Types>
-  void process(Types &&... args) {
-    std::clog << "Attempting to unpack " << sizeof...(args) << " values.\n";
-    (processType(std::forward<Types>(args)), ...);
-  }
-
- private:
-  template<class T>
-  void processType(T &value) {
-    if (is_map<T>::value) {
-      processMap(std::forward<T>(value));
-    } else if (is_container<T>::value) {
-      processArray(std::forward<T>(value));
-    } else {
-      std::clog << "Unpacking value type: " << "Unknown" << '\n';
-    }
-  }
-
-  template<>
-  void processType(int8_t &value) {
-    std::clog << "Unpacking value type: " << "int8_t" << '\n';
-  }
-
-  template<>
-  void processType(int16_t &value) {
-    std::clog << "Unpacking value type: " << "int16_t" << '\n';
-  }
-
-  template<>
-  void processType(int32_t &value) {
-    std::clog << "Unpacking value type: " << "int32_t" << '\n';
-  }
-
-  template<>
-  void processType(int64_t &value) {
-    std::clog << "Unpacking value type: " << "int64_t" << '\n';
-  }
-
-  template<>
-  void processType(uint8_t &value) {
-    std::clog << "Unpacking value type: " << "uint8_t" << '\n';
-  }
-
-  template<>
-  void processType(uint16_t &value) {
-    std::clog << "Unpacking value type: " << "uint16_t" << '\n';
-  }
-
-  template<>
-  void processType(uint32_t &value) {
-    std::clog << "Unpacking value type: " << "uint32_t" << '\n';
-  }
-
-  template<>
-  void processType(uint64_t &value) {
-    std::clog << "Unpacking value type: " << "uint64_t" << '\n';
-  }
-
-  template<>
-  void processType(std::nullptr_t &value) {
-    std::clog << "Unpacking value type: " << "std::nullptr_t" << '\n';
-  }
-
-  template<>
-  void processType(bool &value) {
-    std::clog << "Unpacking value type: " << "boolean" << '\n';
-  }
-
-  template<>
-  void processType(float &value) {
-    std::clog << "Unpacking value type: " << "float" << '\n';
-  }
-
-  template<>
-  void processType(double &value) {
-    std::clog << "Unpacking value type: " << "double" << '\n';
-  }
-
-  template<>
-  void processType(std::string &value) {
-    std::clog << "Unpacking value type: " << "string" << '\n';
-  }
-
-  template<>
-  void processType(std::vector<uint8_t> &value) {
-    std::clog << "Unpacking value type: " << "byte array" << '\n';
-  }
-
-  template<class T>
-  void processArray(T &array) {
-    std::clog << "Unpacking value type: " << "object array" << '\n';
-  }
-
-  template<class T>
-  void processMap(T &map) {
-    std::clog << "Unpacking value type: " << "map" << '\n';
-  }
-};
 }
 
 #endif //CPPACK_PACKER_HPP
